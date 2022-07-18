09283db887b765c731a6e5640118a594a2da7747
MayankSinghChouhan <email.mayanksinghchouhan@gmail.com>
2022-07-18 17:19:38 +0530

    Creating TextForm component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
 
 function App() {
   return (
     <>
+
       {/* <Navbar title = "TextUtil" aboutText = "About TextUtils"/> */}
       {/* <Navbar/> */}
-      <Navbar title = "TextUtil"/>
+      <Navbar title="TextUtil" />
+
+      <div className="container my-3">
+        <TextForm heading="Enter the text to analyze below"/>
+      </div>
+
     </>
   );
 }
